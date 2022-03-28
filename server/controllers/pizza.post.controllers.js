48bdb79a3535a23a4cc9f3f2cf07a5fd0fbbbff4
Farid <faridurielrodriguez@gmail.com>
2022-03-28 15:48:58 -0300

    Controllers finish

--- server/controllers/pizza.post.controllers.js
+++ server/controllers/pizza.post.controllers.js
@@ -1,19 +1,60 @@
-export const getAllPost = (req, res) => {
-    res.send("Hello World");
+import PizzaPost from '../models/PizzaPost.js';
+import mongoose from 'mongoose';
+
+export const getAllPost = async (req, res) => {
+    try {
+        const posts = await PizzaPost.find();
+
+        res.status(200).json(posts);
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 };
 
-export const createPost = (req, res) => {
-    res.send("Hello World");
+export const createPost = async (req, res) => {
+    const { name, description, image, price } = req.body;
+
+    const newPost = new PizzaPost({ name, description, image, price });
+
+    try {
+        await newPost.save();
+
+        res.status(200).json(newPost);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 };
 
-export const getOnePost = (req, res) => {
-    res.send("Hello World");
+export const getOnePost = async (req, res) => {
+    try {
+        const post = await PizzaPost.findById(req.params.id);
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
 };
 
-export const updatePost = (req, res) => {
-    res.send("Hello World");
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { name, description, image, price } = req.body;
+
+        const updatePost = { name, description, image, price, _id: id };
+
+        if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+        await PizzaPost.findByIdAndUpdate(id, updatePost, { new: true });
+
+        res.json(updatePost);
 };
 
-export const deletePost = (req, res) => {
-    res.send("Hello World");
+export const deletePost = async(req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await PizzaPost.findByIdAndDelete(id);
+
+    res.status(201).json({message: "Post deleted successfully"});
 };
